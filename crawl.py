85290c323321d6cc69c3d6e03e93ced30faa820e
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-20 11:03:36 +1000

    repaired the date comparison logic in the crawler

--- crawl.py
+++ crawl.py
@@ -1,9 +1,28 @@
-def activate_crawler(target_directory, date_boundary,url):
+import datetime as dt
+datestring = "2023/01/01"
+minusedtime = dt.datetime(int(datestring.split("/")[0]),int(datestring.split("/")[1]),int(datestring.split("/")[2]))-dt.timedelta(days=9)
+time = dt.datetime(int(datestring.split("/")[0]),int(datestring.split("/")[1]),int(datestring.split("/")[2]))
+print(minusedtime<time)
+
+def activate_crawler(target_directory, date_boundary, url):
+
+    def number_to_date(number):
+        year = number[0:4]
+        month = number[4:6]
+        day = number[6:8]
+        return(year+"/"+month+"/"+day)
+    
+    def compare_dates(this_date,baseline_date,buffer):
+        baseline_date_object_with_buffer = dt.datetime(int(baseline_date.split("/")[0]),int(baseline_date.split("/")[1]),int(baseline_date.split("/")[2]))-dt.timedelta(days=buffer)
+        this_date_object = dt.datetime(int(this_date.split("/")[0]),int(this_date.split("/")[1]),int(this_date.split("/")[2]))
+        return(this_date_object>=baseline_date_object_with_buffer)
+
     import os
     import requests
     from bs4 import BeautifulSoup
     from urllib.parse import urljoin
     from deleter import delete_files_in_folder
+    import datetime as dt
 
     # Directory to save the downloaded files
     download_directory = os.path.join(os.getcwd(), target_directory) #Puts shit in current directory
@@ -25,9 +44,8 @@ def activate_crawler(target_directory, date_boundary,url):
 
         # Check if the link points to a zip file
         if file_url.endswith('.zip'):
-            numberdate_currentdata = int(((file_url.split("/")[-1]).split("_")[-1]).split(".")[0])
-            numberdate_mydata = int((date_boundary.split("/"))[0]+(date_boundary.split("/"))[1]+(date_boundary.split("/"))[2])
-            if(numberdate_currentdata>=(numberdate_mydata-9)):
+            numberdate_selected_zip = number_to_date(((file_url.split("/")[-1]).split("_")[-1]).split(".")[0])
+            if(compare_dates(numberdate_selected_zip,date_boundary)):
                 # Send a GET request to download the file
                 file_response = requests.get(file_url)
 
