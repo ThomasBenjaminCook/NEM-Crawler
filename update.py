9610d0a665acfa0db866a892de462caedb47aa1f
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-23 22:09:59 +1000

    Delete prints

--- update.py
+++ update.py
@@ -6,8 +6,6 @@ from deleter import delete_files_in_folder
 from pvmerge import addPV
 from zip_actions import compress, extract_all
 
-print("Gathering current data.")
-
 if('database_zip.zip' in get_names("zip_output")):
     extract_all("zip_output","csv_output")
     main_database = pd.read_csv("csv_output/database.csv")
@@ -15,19 +13,11 @@ if('database_zip.zip' in get_names("zip_output")):
 else:
     most_recent_date = "0000/00/0000"
 
-print("Crawling NEM for new data.")
-
 activate_crawler("raw_zip_files", most_recent_date,'http://www.nemweb.com.au/Reports/ARCHIVE/Dispatch_SCADA/') #Crawls for all the data beyond the most recent date.
-
-print("Extracting daily data.")
 
 extract_all('raw_zip_files', 'next_layer_zip_files') #Extracts the five min interval zip files from the daily zip files.
 
-print("Extracting 5-minutely data.")
-
 extract_all('next_layer_zip_files', 'raw_csv_files') #Extracts the csv files from the 5 min zip files.
-
-print("Collating the data.")
 
 reduce() #Collates the 5 min zip files into small (approx daily) csv files.
 
@@ -35,11 +25,7 @@ new_data_file_name = merge("processed_csv_files") #Merges the daily zip files.
 
 new_data = pd.read_csv(new_data_file_name).rename({"Unnamed: 0":"Datetime"}, axis=1)
 
-print("Searching for rooftop solar data.")
-
 newdata = addPV(new_data)
-
-print("Merging the data with current database.")
 
 if('database.csv' in get_names("csv_output")):
     align(main_database, new_data).to_csv("csv_output/database.csv", index=False)
@@ -51,5 +37,3 @@ else:
     delete_files_in_folder("processed_csv_files")
     compress("database.csv","database_zip.zip")
     delete_files_in_folder("csv_output")
-
-print("You're up to date.")
